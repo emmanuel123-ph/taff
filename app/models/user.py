3f0b91777584f6cea2a6dfab99f5c63e6881edc2
Laurent Alphonse <laurentalphonsewilfried@gmail.com>
2025-01-30 18:37:27 +0100

    COMMIT

--- app/models/user.py
+++ app/models/user.py
@@ -2,14 +2,14 @@ from sqlalchemy import Column, Integer, String, Boolean, DateTime,ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
 from sqlalchemy.orm import relationship
-Base = declarative_base() # Base de tous les modèles
+from app.models.db.base_class import Base
 class User(Base):
     __tablename__ = "users" # Nom de la table dans la base de données
-    uuid = Column(String, primary_key=True, index=True) # Clé primaire
-    username = Column(String, unique=True, index=True, nullable=False) # Nom d'utilisateur unique
-    email = Column(String, unique=True, index=True, nullable=False) # Email unique
-    phone_number=Column(String,unique=True, nullable=False)
-    hashed_password = Column(String, nullable=False) # Mot de passe haché
+    uuid = Column(String(255), primary_key=True, index=True) # Clé primaire
+    username = Column(String(255),index=True, nullable=False) # Nom d'utilisateur unique
+    email = Column(String(255), unique=True, index=True, nullable=False) # Email unique
+    phone_number=Column(String(255),unique=True, nullable=False)
+    hashed_password = Column(String(255), nullable=False) # Mot de passe haché
     is_deleted = Column(Boolean, default=False)
     is_active = Column(Boolean, default=True) # Statut actif ou non
     created_at = Column(DateTime, default=datetime.utcnow) # Date de création
